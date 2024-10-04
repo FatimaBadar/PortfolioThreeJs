5f72a67d525992480f1e369a8b74f494f9b0192f
Noor Fatima <fatimabadarq@gmail.com>
2024-10-04 16:57:53 +1300

    Animations on scroll

--- main.js
+++ main.js
@@ -22,28 +22,91 @@ renderer.setSize(window.innerWidth, window.innerHeight) //full screen canvas
 //currently camera is at the middle of the scene
 //so more it:
 camera.position.setZ(30); //will give better perspective when we start adding shapes
+camera.position.setX(-3);
 
 renderer.render(scene, camera) //draw
 //rn blank screen, so add objects:
 //geometry: {x,y,z} ppints that make a shape
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } ); //like a wrapping paper
+// const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 ); 
+const material = new THREE.MeshStandardMaterial( { color: 0xffff  } ); //like a wrapping paper
 const torus = new THREE.Mesh( geometry, material ); //applies material to geometry
 scene.add( torus );
 
-const pointLight = new THREE.PointLight(0xffffff)
+//lights
+const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5,5)
 
 const ambientLight = new THREE.AmbientLight(0xffffff) //like bulb light, lighst everything equally
 scene.add(pointLight, ambientLight)
 
 //helpers:
-const lightHelper = new THREE.PointLightHelper(pointLight) //shows the position of pointlight
-const gridHelper = new THREE.GridHelper(200, 50) 
-scene.add(lightHelper, gridHelper)
+// const lightHelper = new THREE.PointLightHelper(pointLight) //shows the position of pointlight
+// const gridHelper = new THREE.GridHelper(200, 50) 
+// scene.add(lightHelper, gridHelper)
 
-//to move around the scene using mouse:
-const controls = new OrbitControls(camera, renderer.domElement)
+// //to move around the scene using mouse:
+// const controls = new OrbitControls(camera, renderer.domElement)
+
+function addStar(){
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+    const star = new THREE.Mesh(geometry, material);
+
+    //randomly positioning stars around the sceen
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+
+    star.position.set(x,y,z)
+    scene.add(star)
+}
+Array(200).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader().load('images/lights.jpg');
+scene.background = spaceTexture
+
+//background
+const objectTexture = new THREE.TextureLoader().load('images/flower.webp');
+const object = new THREE.Mesh(
+    new THREE.BoxGeometry(3,3,3),
+    new THREE.MeshBasicMaterial( {map: objectTexture})
+)
+scene.add(object)
+
+//moon
+const moonTexture = new THREE.TextureLoader().load('images/moon.jpg');
+const bumpyTexture = new THREE.TextureLoader().load('images/normal.jpg');
+
+const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(3, 32, 32),
+    new THREE.MeshStandardMaterial({
+        map: moonTexture,
+        normalMap: bumpyTexture
+    })
+)
+scene.add(moon)
+
+moon.position.z = 30;
+moon.position.setX(-10)
+
+object.position.z = -5;
+object.position.x = 2;
+
+//scroll animation
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+    moon.rotation.x += 0.05;
+    moon.rotation.y += 0.075;
+    moon.rotation.z += 0.05;
+
+    object.rotation.y += 0.01;
+    object.rotation.z += 0.01;
+
+    camera.position.z = t * -0.01;
+    camera.position.x = t * -0.0002;
+    camera.rotation.y = t * -0.0002;
+  }
+document.body.onscroll = moveCamera
+moveCamera()
 
 function animate() { //recursive func that calls render auto to reflect changes to UI as things/ we move
     requestAnimationFrame(animate)
@@ -51,8 +114,10 @@ function animate() { //recursive func that calls render auto to reflect changes 
     torus.rotation.y +=0.005
     torus.rotation.z += 0.01 
 
-    controls.update();
-    
+    moon.rotation.x +=0.005
+
+    // controls.update();
+
     renderer.render(scene, camera)
 }
 animate();
